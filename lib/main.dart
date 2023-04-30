5ed08324567e6785eb6f3af0364c0e9679888b79
Facundo de la Torre <facu17294@gmail.com>
2023-04-30 02:54:54 -0300

    primer commit

---
+++ lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:contador_basico/presentation/screens/counter_screen.dart';
+// import 'package:contador_basico/presentation/screens/counter/counter_screen.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp( const MyApp() );
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+  
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        useMaterial3: true,
+        colorSchemeSeed: Colors.cyan
+      ),
+      home: const CounterFunctionsScreen()
+    );
+  }
+
+}
